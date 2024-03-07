3b79e665d2e024c87e4c571d2ecfd6d3958bba1e
Noor Fatima <160162835+NoorFatima7654@users.noreply.github.com>
2024-03-07 19:14:49 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,18 @@
+//Question no 10 
+
+// Noor Fatima, 2024,03,05
+// This program prints a personal message.
+let myName: string = "Noor Fatima"
+ console.log(`Helo,${myName} are you enjoying a typescipt learning journey?`)
+
+//Question no 11
+ let Names: string[] = ["Mishal","Arish","Huzaifa"];
+ for (let i =0; i <  Names.length; i++)  {
+     console.log(Names[i]);
+ }
+//Question no 12
+let Names: string[] = ["Mishal","Arish","Huzaifa"];
+for (let name of Names)  {
+    console.log(`${name},Are you playing football?` );
+}
+
